1e4c9d235dabebc684cb604fdf3062e19015af02
Danil Gomodril <danil.monets@gmail.com>
2018-10-16 01:00:20 +0300

    First commit

---
+++ eboi/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace eboi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StartStop1 = new System.Windows.Forms.Button();
+            this.StartStop2 = new System.Windows.Forms.Button();
+            this.StartStop3 = new System.Windows.Forms.Button();
+            this.StartStop4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.labelSin = new System.Windows.Forms.Label();
+            this.labelCos = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.rofl1 = new System.Windows.Forms.PictureBox();
+            this.rofl3 = new System.Windows.Forms.PictureBox();
+            this.rofl2 = new System.Windows.Forms.PictureBox();
+            this.rofl4 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rofl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rofl3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rofl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rofl4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartStop1
+            // 
+            this.StartStop1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.StartStop1.Location = new System.Drawing.Point(116, 250);
+            this.StartStop1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.StartStop1.Name = "StartStop1";
+            this.StartStop1.Size = new System.Drawing.Size(284, 28);
+            this.StartStop1.TabIndex = 2;
+            this.StartStop1.Text = "Start";
+            this.StartStop1.UseVisualStyleBackColor = false;
+            this.StartStop1.Click += new System.EventHandler(this.StartStop1_Click);
+            // 
+            // StartStop2
+            // 
+            this.StartStop2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.StartStop2.Location = new System.Drawing.Point(491, 250);
+            this.StartStop2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.StartStop2.Name = "StartStop2";
+            this.StartStop2.Size = new System.Drawing.Size(284, 28);
+            this.StartStop2.TabIndex = 4;
+            this.StartStop2.Text = "Start";
+            this.StartStop2.UseVisualStyleBackColor = false;
+            this.StartStop2.Click += new System.EventHandler(this.StartStop2_Click);
+            // 
+            // StartStop3
+            // 
+            this.StartStop3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.StartStop3.Location = new System.Drawing.Point(116, 502);
+            this.StartStop3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.StartStop3.Name = "StartStop3";
+            this.StartStop3.Size = new System.Drawing.Size(284, 28);
+            this.StartStop3.TabIndex = 6;
+            this.StartStop3.Text = "Start";
+            this.StartStop3.UseVisualStyleBackColor = false;
+            this.StartStop3.Click += new System.EventHandler(this.StartStop3_Click);
+            // 
+            // StartStop4
+            // 
+            this.StartStop4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.StartStop4.Location = new System.Drawing.Point(491, 502);
+            this.StartStop4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.StartStop4.Name = "StartStop4";
+            this.StartStop4.Size = new System.Drawing.Size(284, 28);
+            this.StartStop4.TabIndex = 8;
+            this.StartStop4.Text = "Start";
+            this.StartStop4.UseVisualStyleBackColor = false;
+            this.StartStop4.Click += new System.EventHandler(this.StartStop4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(116, 48);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 178);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(491, 48);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(284, 178);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(116, 302);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(284, 178);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(491, 302);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(284, 178);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // labelSin
+            // 
+            this.labelSin.AutoSize = true;
+            this.labelSin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelSin.Location = new System.Drawing.Point(695, 59);
+            this.labelSin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelSin.Name = "labelSin";
+            this.labelSin.Size = new System.Drawing.Size(66, 25);
+            this.labelSin.TabIndex = 13;
+            this.labelSin.Text = "sin (x)";
+            this.labelSin.Visible = false;
+            // 
+            // labelCos
+            // 
+            this.labelCos.AutoSize = true;
+            this.labelCos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelCos.Location = new System.Drawing.Point(313, 314);
+            this.labelCos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCos.Name = "labelCos";
+            this.labelCos.Size = new System.Drawing.Size(72, 25);
+            this.labelCos.TabIndex = 14;
+            this.labelCos.Text = "cos (x)";
+            this.labelCos.Visible = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 568);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1128, 22);
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // rofl1
+            // 
+            this.rofl1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.rofl1.Location = new System.Drawing.Point(979, 48);
+            this.rofl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rofl1.Name = "rofl1";
+            this.rofl1.Size = new System.Drawing.Size(133, 62);
+            this.rofl1.TabIndex = 16;
+            this.rofl1.TabStop = false;
+            // 
+            // rofl3
+            // 
+            this.rofl3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.rofl3.Location = new System.Drawing.Point(979, 302);
+            this.rofl3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rofl3.Name = "rofl3";
+            this.rofl3.Size = new System.Drawing.Size(133, 62);
+            this.rofl3.TabIndex = 17;
+            this.rofl3.TabStop = false;
+            // 
+            // rofl2
+            // 
+            this.rofl2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.rofl2.Location = new System.Drawing.Point(979, 165);
+            this.rofl2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rofl2.Name = "rofl2";
+            this.rofl2.Size = new System.Drawing.Size(133, 62);
+            this.rofl2.TabIndex = 18;
+            this.rofl2.TabStop = false;
+            // 
+            // rofl4
+            // 
+            this.rofl4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.rofl4.Location = new System.Drawing.Point(979, 418);
+            this.rofl4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rofl4.Name = "rofl4";
+            this.rofl4.Size = new System.Drawing.Size(133, 62);
+            this.rofl4.TabIndex = 19;
+            this.rofl4.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(840, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 29);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Thread 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(840, 182);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 29);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Thread 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.Location = new System.Drawing.Point(840, 314);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 29);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Thread 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label4.Location = new System.Drawing.Point(840, 437);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 29);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Thread 4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 590);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rofl4);
+            this.Controls.Add(this.rofl2);
+            this.Controls.Add(this.rofl3);
+            this.Controls.Add(this.rofl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.labelCos);
+            this.Controls.Add(this.labelSin);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.StartStop4);
+            this.Controls.Add(this.StartStop3);
+            this.Controls.Add(this.StartStop2);
+            this.Controls.Add(this.StartStop1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Using Threads Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rofl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rofl3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rofl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rofl4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button StartStop1;
+        private System.Windows.Forms.Button StartStop2;
+        private System.Windows.Forms.Button StartStop3;
+        private System.Windows.Forms.Button StartStop4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label labelSin;
+        private System.Windows.Forms.Label labelCos;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.PictureBox rofl1;
+        private System.Windows.Forms.PictureBox rofl3;
+        private System.Windows.Forms.PictureBox rofl2;
+        private System.Windows.Forms.PictureBox rofl4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ eboi/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace eboi
+{
+    public partial class Form1 : Form
+    {
+        int b1 = 0, b11 = 0;
+        int b2 = 0, b21 = 0;
+        int b3 = 0, b31 = 0;
+        int b4 = 0, b41 = 0;
+
+        Thread t1;
+        Thread t2;
+        Thread t3;
+        Thread t4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.BackgroundImage = Properties.Resources.yo;
+            this.Icon = Properties.Resources.happy;
+            pictureBox1.BackColor = Color.White;
+            pictureBox2.BackColor = Color.White;
+            pictureBox3.BackColor = Color.White;
+            pictureBox4.BackColor = Color.White;          
+        }
+
+        private void ChangeStatusStrip(dynamic s)
+        {
+            statusStrip1.Items.Clear();
+            statusStrip1.Items.Add(s);
+        }
+
+        private void DrawCircle(int x, int y, int width, int height, Graphics graphic)
+        {
+            Pen pen = new Pen(Color.Red, 5);
+            Graphics circle = graphic;
+            circle.DrawEllipse(pen, x, y, width, height);
+        }
+        private void DrawCircleLoop(int x, int y, int maxDiameter, Graphics graphic)
+        {
+            int i = 0;
+            while (true)
+            {
+                for (; i <= maxDiameter; ++i)
+                {
+                    DrawCircle(x, y, i, i, graphic);
+                    Thread.Sleep(50);
+                    graphic.Clear(Color.White);
+                }
+                for (; i >= 0; --i)
+                {
+                    DrawCircle(x, y, i, i, graphic);
+                    Thread.Sleep(50);
+                    graphic.Clear(Color.White);
+                }
+            }
+            
+        }
+        private void DrawCircleLoopThread(dynamic o)
+        { 
+            DrawCircleLoop(o[0], o[1], o[2], o[3]);
+        }
+
+        private void StartStop1_Click(object sender, EventArgs e)
+        {
+            if (b11 == 0)
+            {
+                StartStop1.Text = "Stop";
+                t1 = new Thread(DrawCircleLoopThread);
+                Graphics g1 = pictureBox1.CreateGraphics();
+                dynamic[] lol = { pictureBox1.Width / 2 - 40, pictureBox1.Height / 2 - 40, 85, g1 };
+                t1.Start(lol);
+                b11 = 1;
+                b1 = 1;
+                ChangeStatusStrip("Thread 1 was started. ");
+                rofl1.BackColor = Color.Green;
+                Graphics kek = rofl1.CreateGraphics();
+            }
+            else if (b1 == 0)
+            {              
+                StartStop1.Text = "Stop";
+                t1.Resume();
+                b1 = 1;
+                ChangeStatusStrip("Thread 1 was resumed. ");
+                rofl1.BackColor = Color.Green;
+            }
+            else if (b1 == 1)
+            {
+                
+                t1.Suspend();
+                b1 = 0;
+                StartStop1.Text = "Start";
+                ChangeStatusStrip("Thread 1 was stopped. ");
+                rofl1.BackColor = Color.Red;
+            }
+        }
+
+        private void DrawXYC(dynamic g)
+        {      
+            Pen GreenPen;
+            GreenPen = new Pen(Color.Green);
+
+            Point Centr;
+            int centrX = pictureBox2.Width;
+            int centrY = pictureBox2.Height; 
+            Centr = new Point(centrX / 2, centrY / 2);
+
+            Point KX1, KX2;
+            KX1 = new Point(0, centrY / 2);
+            KX2 = new Point(centrX, centrY / 2);
+            g.DrawLine(GreenPen, KX1, KX2);
+
+            Point KY1, KY2;
+            KY1 = new Point(centrX / 2, 0);
+            KY2 = new Point(centrX / 2, centrY);
+
+            g.DrawLine(GreenPen, KY1, KY2);
+        }
+        private void DrawSinX(int cx, int cy, int cw, Graphics g)
+        {
+            while (true)
+            {
+                PointF[] p1 = new PointF[cx];
+                for (int i = 0; i < cx; ++i)
+                {
+                    p1[i].X = i;
+                    p1[i].Y = (float)((cy / 2) * (1 - Math.Sin(i * cw * Math.PI / (cx - 1))));
+
+                    DrawXYC(g);
+                    g.DrawLines(Pens.Red, p1);
+
+                    Thread.Sleep(10);
+
+                    g.Clear(Color.White);
+                }
+                p1 = null;
+                PointF[] p2 = new PointF[cx];
+                for (int i = cx - 1; i >= 0; --i)
+                {
+                    p2[i].X = i;
+                    p2[i].Y = (float)((cy / 2) * (1 - Math.Sin(i * cw * Math.PI / (cx - 1))));
+
+                    DrawXYC(g);
+                    g.DrawLines(Pens.Red, p2);
+
+                    Thread.Sleep(10);
+
+                    g.Clear(Color.White);
+                }
+                p2 = null;
+            }
+        }
+        private void DrawSinXThread(dynamic o)
+        {
+            DrawSinX(o[0], o[1], o[2], o[3]);
+        }  
+
+        private void StartStop2_Click(object sender, EventArgs e)
+        {
+            labelSin.Visible = true;
+            if (b21 == 0)
+            {
+                StartStop2.Text = "Stop";
+                t2 = new Thread(DrawSinXThread);
+                Graphics g2 = pictureBox2.CreateGraphics();
+                dynamic[] lol = { pictureBox2.Width, pictureBox2.Height, 8, g2 };
+                t2.Start(lol);
+                b21 = 1;
+                b2 = 1;
+                ChangeStatusStrip("Thread 2 was started. ");
+                rofl2.BackColor = Color.Green;
+            }
+            else if (b2 == 0)
+            {
+                StartStop2.Text = "Stop";
+                t2.Resume();
+                b2 = 1;
+                ChangeStatusStrip("Thread 2 was resumed. ");
+                rofl2.BackColor = Color.Green;
+            }
+            else if (b2 == 1)
+            {
+                t2.Suspend();
+                b2 = 0;
+                StartStop2.Text = "Start";
+                ChangeStatusStrip("Thread 2 was stopped. ");
+                rofl2.BackColor = Color.Red;
+            }
+        }
+
+        private void DrawCosX(int cx, int cy, int cw, Graphics g)
+        {
+            while (true)
+            {
+                PointF[] p1 = new PointF[cx];
+                for (int i = 0; i < cx; ++i)
+                {
+                    p1[i].X = i;
+                    p1[i].Y = (float)((cy / 2) * (1 - Math.Cos(i * cw * Math.PI / (cx - 1))));
+
+                    DrawXYC(g);
+                    g.DrawLines(Pens.Red, p1);
+
+                    Thread.Sleep(10);
+
+                    g.Clear(Color.White);
+                }
+                p1 = null;
+                PointF[] p2 = new PointF[cx];
+                for (int i = cx - 1; i >= 0; --i)
+                {
+                    p2[i].X = i;
+                    p2[i].Y = (float)((cy / 2) * (1 - Math.Cos(i * cw * Math.PI / (cx - 1))));
+
+                    DrawXYC(g);
+                    g.DrawLines(Pens.Red, p2);
+
+                    Thread.Sleep(10);
+
+                    g.Clear(Color.White);
+                }
+                p2 = null;
+            }
+        }
+        private void DrawCosXThread(dynamic o)
+        {
+            DrawCosX(o[0], o[1], o[2], o[3]);
+        }
+
+        private void StartStop3_Click(object sender, EventArgs e)
+        {
+            labelCos.Visible = true;
+            if (b31 == 0)
+            {
+                StartStop3.Text = "Stop";
+                t3 = new Thread(DrawCosXThread);
+                Graphics g3 = pictureBox3.CreateGraphics();
+                dynamic[] lol = { pictureBox3.Width, pictureBox3.Height, 8, g3 };
+                t3.Start(lol);
+                b31 = 1;
+                b3 = 1;
+                ChangeStatusStrip("Thread 3 was started. ");
+                rofl3.BackColor = Color.Green;
+            }
+            else if (b3 == 0)
+            {
+                StartStop3.Text = "Stop";
+                t3.Resume();
+                b3 = 1;
+                ChangeStatusStrip("Thread 3 was resumed. ");
+                rofl3.BackColor = Color.Green;
+            }
+            else if (b3 == 1)
+            {
+                t3.Suspend();
+                b3 = 0;
+                StartStop3.Text = "Start";
+                ChangeStatusStrip("Thread 3 was stopped. ");
+                rofl3.BackColor = Color.Red;
+            }
+            
+        }
+
+        private void Func4Rofl(dynamic g)
+        {
+            Random random = new Random();
+            int height = pictureBox4.Height;
+            int width = pictureBox4.Width;
+
+            Pen pen = new Pen(Color.Green, 2);
+
+            int v = 2; 
+            double x = random.Next(30, width - 30);  
+            double y = random.Next(30, height - 30);  
+            double a = random.Next(0, 360);   
+
+            double vx = v * Math.Cos(a);     
+            double vy = v * Math.Sin(a);
+
+            while (true)
+            {
+                g.DrawEllipse(new Pen(Color.White, 2), Convert.ToInt32(Math.Round(x)), Convert.ToInt32(Math.Round(y)), 30, 30);
+
+                x += vx; 
+                y += vy;
+
+                if (x < 1)
+                {
+                    vx = -vx;
+                }
+                if (x > width - 30)
+                {
+                    vx = -vx;  
+                }
+                if (y < 1)
+                {
+                    vy = -vy;
+                }
+                if (y > height - 30)
+                {
+                    vy = -vy;
+                }
+
+                g.DrawEllipse(pen, Convert.ToInt32(Math.Round(x)), Convert.ToInt32(Math.Round(y)), 30, 30);
+                Thread.Sleep(10);
+            }
+        }
+
+        private void StartStop4_Click(object sender, EventArgs e)
+        {
+            if (b41 == 0)
+            {
+                StartStop4.Text = "Stop";
+                t4 = new Thread(Func4Rofl);
+                Graphics g4 = pictureBox4.CreateGraphics();
+                t4.Start(g4);
+                b41 = 1;
+                b4 = 1;
+                ChangeStatusStrip("Thread 4 was started. ");
+                rofl4.BackColor = Color.Green;
+            }
+            else if (b4 == 0)
+            {
+                StartStop4.Text = "Stop";
+                t4.Resume();
+                b4 = 1;
+                ChangeStatusStrip("Thread 4 was resumed. ");
+                rofl4.BackColor = Color.Green;
+            }
+            else if (b4 == 1)
+            {
+                t4.Suspend();
+                b4 = 0;
+                StartStop4.Text = "Start";
+                ChangeStatusStrip("Thread 4 was stopped. ");
+                rofl4.BackColor = Color.Red;
+            }
+        }
+    }
+}
